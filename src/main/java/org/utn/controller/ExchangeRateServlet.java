aa7181b7c58bec2ab3780fe238b967ab0e3c04e4
rustam sagaddinov <rustiksagad@gmail.com>
2025-03-22 12:12:45 -0300

    create servlet for POST a new ExchangeRate

--- src/main/java/org/utn/controller/ExchangeRateServlet.java
+++ src/main/java/org/utn/controller/ExchangeRateServlet.java
@@ -11,6 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -18,6 +21,7 @@ public class ExchangeRateServlet extends HttpServlet {
 
     private final ExchangeRateDAO exchangeRateDAO = new ExchangeRateDAO();
     private final Gson gson = new Gson();
+    private final CurrencyDAO currencyDAO = new CurrencyDAO();
 
 
     @Override
@@ -54,6 +58,57 @@ public class ExchangeRateServlet extends HttpServlet {
 
             resp.getWriter().write(gson.toJson(responseMap));
 
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal server error");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            System.out.println("Parameters received from the request");
+            Map<String, String[]> parameters = req.getParameterMap();
+            parameters.forEach((key, value) -> System.out.println(key + ": " + Arrays.toString(value)));
+
+            if (!parameters.containsKey("baseCurrencyCode") ||
+                    !parameters.containsKey("targetCurrencyCode") ||
+                    !parameters.containsKey("rate")) {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing required parameters");
+                return;
+            }
+
+            String baseCurrencyCode = parameters.get("baseCurrencyCode")[0];
+            String targetCurrencyCode = parameters.get("targetCurrencyCode")[0];
+            String rateStr = parameters.get("rate")[0];
+
+
+            if (baseCurrencyCode == null || targetCurrencyCode == null || rateStr == null) {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing required form fields");
+                return;
+            }
+            BigDecimal rate;
+            try {
+                rate = new BigDecimal(rateStr);
+            } catch (NumberFormatException e) {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid rate format");
+                return;
+            }
+            Currency baseCurrency = currencyDAO.getCurrencyByCode(baseCurrencyCode);
+            Currency targetCurrency = currencyDAO.getCurrencyByCode(targetCurrencyCode);
+            if (baseCurrency == null || targetCurrency == null) {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "One or both currency not found");
+                return;
+            }
+            if (exchangeRateDAO.getExchangeRateByCodes(baseCurrencyCode, targetCurrencyCode) != null) {
+                resp.sendError(HttpServletResponse.SC_CONFLICT, "Exchange rate already exists");
+                return;
+            }
+            ExchangeRate newExchangeRate = exchangeRateDAO.addExchangeRate(baseCurrency, targetCurrency, rate);
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+            resp.getWriter().write(gson.toJson(buildExchangeRateMap(newExchangeRate)));
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal server error");
--- src/main/java/org/utn/dao/ExchangeRateDAO.java
+++ src/main/java/org/utn/dao/ExchangeRateDAO.java
@@ -4,6 +4,7 @@ import org.utn.model.Currency;
 import org.utn.model.ExchangeRate;
 
 import java.math.BigDecimal;
+import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -59,7 +60,6 @@ public class ExchangeRateDAO {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-
         return null;
     }
 
@@ -95,6 +95,43 @@ public class ExchangeRateDAO {
 
         return null;
     }
+
+
+    public ExchangeRate addExchangeRate(Currency baseCurrency, Currency targetCurrency, BigDecimal rate) {
+        if (baseCurrency == null || targetCurrency == null) {
+            return null;
+        }
+        if (getExchangeRateByCodes(baseCurrency.getCode(), targetCurrency.getCode()) != null) {
+            return null;
+        }
+
+        String sql = "INSERT INTO ExchangeRates (base_currency_id, target_currency_id, rate) VALUES (?, ?, ?)";
+
+        try (Connection conn = DriverManager.getConnection(URL);
+             PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            pstmt.setInt(1, baseCurrency.getId());
+            pstmt.setInt(2, targetCurrency.getId());
+            pstmt.setBigDecimal(3, rate);
+
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows == 0) {
+                return null;
+            }
+
+            try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    int id = generatedKeys.getInt(1);
+                    return new ExchangeRate(id, baseCurrency, targetCurrency, rate);
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return null;
+    }
+
 
 }
 
